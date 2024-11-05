c8da9358f72671182c86aaf8c8813e6ad31fe7d2
WTFPUn <punn_namw@hotmail.com>
2024-11-05 21:17:18 +0700

    add hardware shifting function

--- hardware.py
+++ hardware.py
@@ -1,8 +1,26 @@
+import gpiod
+from config import X_AXIS_PIN, Y_AXIS_PIN
+
+chip = gpiod.Chip("gpiochip4")
+# set the line to output
+# line = chip.get_line(PWM_PIN)
+# line.request(consumer="pwm", type=gpiod.LINE_REQ_DIR_OUT)
+
+x_line = chip.get_line(X_AXIS_PIN)
+y_line = chip.get_line(Y_AXIS_PIN)
+
+
 def shift_camera():
     """
-    Function to shift the camera view using hardware control.
+    Function to shift x axis(camera)
     """
-    print("Shifting camera to a new position.")
-    # Implement hardware control to shift camera
-    # For example, using GPIO pins to control motors
-    # GPIO.output(pin_number, GPIO.HIGH)
+    x_line.set_value(1)
+    x_line.set_value(0)
+
+
+def shift_raft():
+    """
+    Function to shift y axis(raft)
+    """
+    y_line.set_value(1)
+    y_line.set_value(0)
